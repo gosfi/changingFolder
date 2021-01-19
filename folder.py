41378de26ac0932bd709106745a83c83a7ca6255
gosfi <samLef12@outlook.com>
2021-01-19 13:23:37 -0500

    removed my specific folder location

--- folder.py
+++ folder.py
@@ -9,17 +9,19 @@ class MyHandler(FileSystemEventHandler):
     def change_folder(self):
         for filename in os.listdir(folder_to_track):
             src = folder_to_track + "\\" + filename
+            #insert as much file type as you want here
             if filename.endswith(".pdf") or filename.endswith(".epub"):
-                folder_destination = "C:\\Users\\Samuel\\Desktop\\books"
+                folder_destination = ""
             elif filename.endswith(".jpeg") or filename.endswith(".png") or filename.endswith(".jpg"):
-                folder_destination = "C:\\Users\\Samuel\\Desktop\\images"
+                folder_destination = ""
             else:
+                #if none of the types work, don't move this file
                 folder_destination = folder_to_track
             new_destination = folder_destination + "\\" + filename
             os.rename(src, new_destination)
 
-
-folder_to_track = "C:\\Users\\Samuel\\Downloads"
+#insert which folder to look at
+folder_to_track = ""
 folder_destination = ""
 handler = MyHandler()
 handler.change_folder()
